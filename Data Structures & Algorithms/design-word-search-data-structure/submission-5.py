b0edd44eb665ab5134f3c6295845817c988080f8
Areen Patil <75596662+Sannainmf@users.noreply.github.com>
2026-07-16 16:13:00 -0400

    Add: design-word-search-data-structure - submission-5

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-5.py
@@ -0,0 +1,48 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+class WordDictionary:
+
+    def __init__(self):
+        
+        self.trie = TrieNode()
+
+    def addWord(self, word: str) -> None:
+
+        trie = self.trie
+
+        for char in word:
+            if char not in trie.children:
+                trie.children[char] = TrieNode()
+            
+            trie = trie.children[char]
+
+        trie.word = True
+        
+
+    def search(self, word: str) -> bool:
+
+        def dfs(j, root):
+            trie = root
+
+            for i in range(j, len(word)):
+
+                char = word[i]
+
+                if char == ".":
+                    for child in trie.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if char not in trie.children:
+                        return False
+                    
+                    trie = trie.children[char]
+
+            return trie.word
+                
+        return dfs(0, self.trie)
+

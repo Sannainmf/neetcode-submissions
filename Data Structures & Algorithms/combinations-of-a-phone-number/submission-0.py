adfec0f223bd79d07a43e37d3ae8bbc68036115f
Areen Patil <75596662+Sannainmf@users.noreply.github.com>
2026-07-21 16:42:36 -0400

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+
+        hsh = {
+            '2' : 'abc',
+            '3' : 'def',
+            '4' : 'ghi',
+            '5' : 'jkl',
+            '6' : 'mno',
+            '7' : 'pqrs',
+            '8' : 'tuv',
+            '9' : 'wxyz'
+        }
+
+        if digits == "":
+            return []
+
+        res, sol = [], []
+
+        def dfs(start):
+            if len(sol) == len(digits):
+                res.append(''.join(sol))
+                return
+
+            for char in hsh[digits[start]]:
+                sol.append(char)
+                dfs(start + 1)
+                sol.pop()
+
+        dfs(0)
+        return res
+        

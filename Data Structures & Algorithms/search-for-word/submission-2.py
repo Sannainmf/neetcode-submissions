eb0ca4585d7da6576c1a3c5f80a3b9473e3e3304
Areen Patil <75596662+Sannainmf@users.noreply.github.com>
2026-07-22 12:06:54 -0400

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.py
@@ -0,0 +1,59 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+
+        res, sol = [], []
+
+        """
+        we have 4 directions
+        we cant go out of bounds
+        we cant go to letter on same path, only remaining edges
+        """ 
+
+        used = [[False] * len(board[0]) for i in range(len(board))]
+            
+        def dfs(row, col, start):
+            if board[row][col] != word[start]:
+                return
+
+            if start == len(word) - 1:
+                sol.append(board[row][col])
+                res.append(''.join(sol[:]))
+                sol.pop()
+                return
+
+            sol.append(board[row][col])
+            used[row][col] = True
+
+            if row > 0 and not used[row - 1][col]: #goes up
+                dfs(row - 1, col, start + 1)
+
+            if row < len(board) - 1 and not used[row + 1][col]: #goes down
+                dfs(row + 1, col, start + 1)
+
+            if col > 0 and not used[row][col - 1]:
+                dfs(row, col - 1, start + 1)
+
+
+            if col < len(board[0]) - 1 and not used[row][col + 1]:
+                dfs(row, col + 1, start + 1)
+
+            used[row][col] = False
+            sol.pop()
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == word[0]:
+                    dfs(i, j, 0)
+        
+        return word in res
+
+        
+
+        
+
+
+            
+
+
+
+        
